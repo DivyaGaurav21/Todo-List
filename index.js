57d0dced2fac5bc43241f605ca16189680366e82
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-11-11 02:01:50 +0530

    setting ejs engine and thinking on accesing static file

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const app = express();
 app.use('/' , require('./routers/index'))
 
 
+//setting view engine as ejs
+app.set('view engine','ejs');
+//setting path for views directory
+app.set('views','./views');
 
+//static file , which is present in assets directory
+app.use(express.static('assets'));
 
 
 app.listen(port , (err) => {
--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
+//setup express router
+const router = express.Router();
 const homeController = require('../controllers/home_controller')
 
-const router = express.Router();
 
 console.log('router loaded')
 
 router.get('/' , homeController.home);
+router.use('/users' , require('./user'))
 
 
 module.exports = router ;
