5633d0722b2d6eff1cb9942b22fe301ff98b3b1b
Divya Gaurav <divyag.ug16.ee@nitp.ac.in>
2022-11-11 13:43:43 +0530

    final UI for displaying dynamic data by checking with static data

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,41 @@
-module.exports.home = (req , res) => {
+// const TodoLists = require('../models/todo_list')
+
+
+//you can see on color on ui i store color in Array
+let colors = {
+   personal : 'darkgreen',
+   work : 'darkmagenta',
+   school : 'darkorange',
+   cleaning : 'darkblue',
+   other : 'darkcyan',
+   study : 'grey' ,
+   'n/a' : 'red',
+} 
+
+var todo = [
+   {
+      description : "shopping for festival" ,
+      date : "2021 , 11, 28",
+      category: 'work'
+   },
+   {
+      description : "playetrsg for festival" ,
+      date : "2021 , 11, 21",
+      category: 'study'
+   },
+   {
+      description : "pdfsdgfh for festival" ,
+      date : "2021 , 11, 18",
+      category: 'school'
+   }
+]
+
+module.exports.home=(req , res) => {
+
    return res.render('home' , {
-      title:'TodoHone'
+      title : 'home' ,
+      todoList : todo ,
+      color_list : colors
+
    })
 }
